3d072ac8988e5c9b2f80e60c86cd5fbc76b8b5a8
coleandreoli <coleandreoli@gmail.com>
2025-09-18 17:08:48 +0200

    env and readme

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # eodp
+
 EODP main repository
+
+## Installation
+
+### Prerequisites
+
+- Python 3.6 or higher
+- pip (Python package installer)
+
+### Install from source
+
+1. Clone the repository:
+
+```bash
+git clone git@github.com:coleandreoli/eodp_students.git
+cd eodp_students-master
+```
+
+2. Install the package and dependencies:
+
+```bash
+pip install -e .
+```
+
+Alternatively, you can install the requirements directly:
+
+```bash
+pip install -r requirements.txt
+```
+
+### Dependencies
+
+The package requires the following main dependencies:
+
+- numpy
+- matplotlib
+- netCDF4
--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -1,11 +1,21 @@
 # MAIN FUNCTION TO CALL THE L1B MODULE
 
+import os
 from l1b.src.l1b import l1b
 
-# Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r"/home/cole/uc3m/earth_observation"
-indir = r"/home/cole/uc3m/earth_observation/input"
-outdir = r"/home/cole/uc3m/earth_observationy/outputs"
+# Get directories from environment variables
+try:
+    auxdir = os.environ["EODP_AUXDIR"]
+    indir = os.environ["EODP_INDIR"]
+    outdir = os.environ["EODP_OUTDIR"]
+except KeyError as e:
+    missing_var = e.args[0]
+    print(f"Error: Environment variable {missing_var} is not set.")
+    print("Please set the following environment variables:")
+    print("  export EODP_AUXDIR=/path/to/earth_observation")
+    print("  export EODP_INDIR=/path/to/earth_observation/input")
+    print("  export EODP_OUTDIR=/path/to/earth_observation/outputs")
+    exit(1)
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
